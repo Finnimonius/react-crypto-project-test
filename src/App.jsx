558e9162fa4daf58e71cc342fcf29880fac7e8ce
Aleksandr Zykov <Finnimonius97@gmail.com>
2025-08-28 18:57:42 +0400

    Added cards and fake api calls

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Layout } from 'antd';
 import AppHeader from './components/layout/AppHeader';
 import AppSider from './components/layout/AppSider';
-import { AppContent } from "./components/layout/AppContent";
+import AppContent from './components/layout/AppContent';
 
 function App() {
   return (
--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -5,8 +5,9 @@ const contentStyle = {
   minHeight: 'calc(100vh - 60px)',
   color: '#fff',
   backgroundColor: '#001529',
+  padding: '1rem'
 };
 
-export default function AppContent () {
+export default function AppContent() {
     return (<Layout.Content style={contentStyle}>Content</Layout.Content>)
 }
